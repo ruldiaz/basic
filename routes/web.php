94858ce9ff4badf977ed44f95732f39d2dec2e9c
RaulDiaz <rulhdiaz@gmail.com>
2025-02-17 14:57:26 -0600

    first commit laravel project v10

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Demo\DemoController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::controller(DemoController::class)->group(function(){
+    Route::get('/about', 'Index');
+    Route::get('/contact', 'ContactMethod');
+});
+
