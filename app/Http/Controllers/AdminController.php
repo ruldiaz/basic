d4553389c6efeebef942eab1deb434ba7c51fad1
RaulDiaz <rulhdiaz@gmail.com>
2025-03-05 13:27:21 -0600

    profile

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -17,5 +18,11 @@ class AdminController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/login');
-    }
+    }// End method
+
+    public function Profile() {
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile_view', compact('adminData'));
+    }// End method
 }
